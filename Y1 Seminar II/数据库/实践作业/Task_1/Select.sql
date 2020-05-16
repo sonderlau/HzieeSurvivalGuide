62c8dfed1d8278367ec1bb5a1c528cb759ffe1e3
Ge15emium <liuless26@gmail.com>
2020-05-16 15:48:07 +0800

    :pencil: 作业答案修改
     by gyl
    提供的官方参考答案

--- Y1 Seminar II/数据库/实践作业/Task_1/Select.sql
+++ Y1 Seminar II/数据库/实践作业/Task_1/Select.sql
@@ -1,17 +1,38 @@
-select ROUTENAME 线路名称,
-       ROUTETYPENAME 线路类别,
-       D2.UNINAME 所属单位,
-       NUM2.UNINAME 所属车队,
-       VALUE 线路车辆类型
-from G_ROUTENUM,
-     G_ROUTETYPE,
-          G_UNITNUM D1 JOIN G_UNITNUM D2
-         ON D1.PARENTNUM = D2.UNITNUM and D2.PARENTNUM = '00',
-     G_UNITNUM NUM1 JOIN G_UNITNUM NUM2
-         ON NUM1.PARENTNUM = NUM2.UNITNUM and NUM2.PARENTNUM != '00' and NUM2.PARENTNUM != '00000000',
-     G_DICINFO
-where
-      G_ROUTETYPE.ROUTETYPE = G_ROUTENUM.ROUTETYPE
-  and G_DICINFO.DICNAME = 'ROUTEBUSTYPE'
-  and G_DICINFO.CODE = G_ROUTENUM.ROUTETYPE
+select a.ROUTENAME 线路名称,
+       b.ROUTETYPENAME 线路类别,
+       c2.UNINAME 所属单位,
+       c1.UNINAME 所属车队,
+       d.VALUE 线路车辆类型
+  from G_ROUTENUM a,
+       g_Routetype b,
+       g_Unitnum c1,
+       g_Unitnum c2,
+       g_dicinfo d
+ where a.routetype = b.routetype
+   and substr(a.routenum, 1, 4) = c1.unitnum
+   and substr(a.routenum, 1, 2) = c2.unitnum
+   and a.routebustype = d.code
+   and d.dicname = 'ROUTEBUSTYPE'
 ;
+
+
+select a.ROUTENAME,
+       b.ROUTETYPENAME,
+       c3.UNINAME,
+       c2.UNINAME,
+       d.VALUE
+  from G_ROUTENUM  a,
+       g_Routetype b,
+       g_Unitnum   c1,
+       g_Unitnum   c2,
+       g_unitnum   c3,
+       g_dicinfo   d
+ where a.routenum = c1.unitnum
+   and a.routetype = b.routetype
+   and c1.parentnum = c2.unitnum
+   and c2.parentnum = c3.unitnum
+   and a.routebustype = d.code
+   and d.dicname = 'ROUTEBUSTYPE'
+;
+
+
